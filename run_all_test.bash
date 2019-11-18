382b6a352cb61071701432755e861bdf25827035
Aidous <2220170232@bit.edu.cn>
2019-11-18 08:38:55 +0800

    update

--- run_all_test.bash
+++ run_all_test.bash
@@ -4,11 +4,10 @@ set -e
 SCRIPTS_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 function run_all_test() {
+    source "${SCRIPTS_PATH}/cyber/setup.bash"
     cd apollo/bin
     ls
-    source "${SCRIPTS_PATH}/cyber/setup.bash"
     declare -i file_count
-    
     file_count=0
     for file in `ls *test`
     do 
